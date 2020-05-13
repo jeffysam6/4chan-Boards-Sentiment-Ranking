d4bac552e5726f02167f46d01caef02029e042af
jeffysam6 <jeffysam02@gmail.com>
2020-05-13 23:28:37 +0530

    gotta store boards and page data

--- Backend/data.py
+++ Backend/data.py
@@ -1,5 +1,6 @@
 from time import sleep
 import requests
+import json
 
 
 boards_endpoint = "https://a.4cdn.org/boards.json"
@@ -7,4 +8,11 @@ boards_endpoint = "https://a.4cdn.org/boards.json"
 
 boards_json = requests.get(boards_endpoint)
 
-print(boards_json.json())
+boards_json = boards_json.json()
+
+print(boards_json["boards"])
+
+print("All Boards")
+
+for board in (boards_json["boards"]):
+    print(board["title"]," ",board["pages"])
