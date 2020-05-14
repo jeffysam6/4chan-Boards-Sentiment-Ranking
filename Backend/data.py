ee0ebf4a2c208d2b13c2b7398a5a87c6db9fd795
jeffysam6 <jeffysam02@gmail.com>
2020-05-14 23:31:51 +0530

    loaded dataset and will have to scrape 4chan board url

--- Backend/data.py
+++ Backend/data.py
@@ -14,5 +14,20 @@ print(boards_json["boards"])
 
 print("All Boards")
 
+boards = []
+
 for board in (boards_json["boards"]):
     print(board["title"]," ",board["pages"])
+    boards.append(board)
+
+for board in boards:
+    for page in range(1,11):
+        # print(board["title"],page)
+        sleep(1)
+        boards_endpoint = f"https://a.4cdn.org/{board['title']}/{page}.json"
+        print(board["title"],page,boards_endpoint)
+        
+        # boards_json = requests.get(boards_endpoint)
+        # if(str(boards_json.status_code) == '404'):
+        #     break
+        # print(boards_json.status_code)
--- Backend/model.py
+++ Backend/model.py
@@ -2,6 +2,10 @@ import torch
 import pandas as pd
 from sklearn.model_selection import train_test_split
 
+from transformers import BertTokenizer
+from torch.utils.data import TensorDataset
+
+
 df = pd.read_csv("smile-dataset/smile-annotations-final.csv",
                     names = ['id','text','category'])
 
@@ -49,4 +53,47 @@ df.loc[X_val,"data_type"] = 'test'
 
 # print(df.head())
 
-print(df.groupby(['category','label','data_type']).count()) #summary
+# print(df.groupby(['category','label','data_type']).count()) #summary
+
+
+tokenizer = BertTokenizer.from_pretrained(
+                        'bert-base-uncased',
+                        do_lower_case = True
+)
+
+encoded_data_train = tokenizer.batch_encode_plus(
+                        df[df.data_type == 'train'].text.values, #text values 
+                        add_special_tokens = True,                 #token to indicate sentences
+                        return_attention_mask = True,               #returns position of masking
+                        pad_to_max_length = True,                   #pads 0 to make size same
+                        max_length = 256,                           #assumed max length of a tweet
+                        return_tensors = 'pt'                           #pt for pytorch
+)
+
+encoded_data_val = tokenizer.batch_encode_plus(
+                        df[df.data_type == 'test'].text.values, #text values 
+                        add_special_tokens = True,                 #token to indicate sentences
+                        return_attention_mask = True,               #returns position of masking
+                        pad_to_max_length = True,                   #pads 0 to make size same
+                        max_length = 256,                           #assumed max length of a tweet
+                        return_tensors = 'pt'                           #pt for pytorch
+)
+
+# print(df.data_type.unique())
+# print(encoded_data_val.keys())
+
+input_ids_train = encoded_data_train['input_ids']
+attention_masks_train = encoded_data_train['attention_mask']
+labels_train = torch.tensor(df[df.data_type=="train"].label.values)
+
+input_ids_val = encoded_data_val['input_ids']
+attention_masks_val = encoded_data_val['attention_mask']
+labels_val = torch.tensor(df[df.data_type=="test"].label.values)
+
+dataset_train = TensorDataset(input_ids_train,attention_masks_train,
+                                labels_train)
+
+dataset_val = TensorDataset(input_ids_val,attention_masks_val,
+                                labels_val)
+
+print(len(dataset_train),len(dataset_val))
